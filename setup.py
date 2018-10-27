f4f4b69279e111ca54d169eb739ac450c65a39dd
Richard Bann <richardbann@gmail.com>
2018-10-27 12:12:32 +0200

    Version 0.5.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
-VERSION = '0.4.0'
+VERSION = '0.5.0'
 
 setup(
     name='gdockutils',
