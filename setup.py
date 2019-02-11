7485efe4a39c939945bd3a4f749e07bb7dae5642
Richard Bann <richardbann@gmail.com>
2019-02-11 17:44:10 +0100

    black setup.py

--- setup.py
+++ setup.py
@@ -5,29 +5,28 @@ import io
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
 
-VERSION = '0.5.16'
+VERSION = "0.5.16"
 
 setup(
-    name='gdockutils',
-    description='Galaktika Solutions - Docker Utilities',
+    name="gdockutils",
+    description="Galaktika Solutions - Docker Utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=VERSION,
-    url='https://github.com/galaktika-solutions/gDockUtils',
-    author='Richard Bann',
-    author_email='richardbann@gmail.com',
+    url="https://github.com/galaktika-solutions/gDockUtils",
+    author="Richard Bann",
+    author_email="richardbann@gmail.com",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6'
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
     ],
-    license='MIT',
-    packages=['gdockutils'],
-    install_requires=[
-        'click >= 7.0',
-    ],
+    license="MIT",
+    packages=["gdockutils"],
+    install_requires=["click >= 7.0"],
     extras_require={
         "dev": [
             "coverage >= 4.5.2",
@@ -35,11 +34,11 @@ setup(
             "sphinx_rtd_theme >= 0.4.2",
             "twine >= 1.12.1",
             "wheel >= 0.32.3",
-        ],
+        ]
     },
     entry_points={
-        'console_scripts': [
-            'gstack=gdockutils.main:cli',
+        "console_scripts": [
+            "gstack=gdockutils.main:cli",
             # 'ensure_db=gdockutils.cli:ensure_db',
             # 'backup=gdockutils.cli:backup',
             # 'restore=gdockutils.cli:restore',
@@ -50,6 +49,6 @@ setup(
             # 'readsecret_ui=gdockutils.ui:readsecret_ui',
             # 'backup_ui=gdockutils.ui:backup_ui',
             # 'restore_ui=gdockutils.ui:restore_ui',
-        ],
-    }
+        ]
+    },
 )
